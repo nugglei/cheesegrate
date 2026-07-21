cec117ae5a11c782a754d663a04a320d9c94d7da
nugglei <itscdsgvlogs@gmail.com>
2026-07-21 03:43:30 -0500

    added retro racetrack

--- lib/swiftMaps.ts
+++ lib/swiftMaps.ts
@@ -9,7 +9,7 @@ export type SwiftMap = {
 }
 
 export const swiftMaps: SwiftMap[] = [
-  { name: "Grassy Greens", dataVer: "1", stars: { one: 6.1, two: 5.449, three: 5.427 } },
+  { name: "Grassy Greens", dataVer: "1", stars: { one: 6.1, two: 5.459, three: 5.417 } },
   { name: "Terracotta Badlands", dataVer: "2", stars: { one: 8.2, two: 7.92, three: 7.826 } },
   { name: "Sapphire Cliffs", dataVer: "3", stars: { one: 19.1, two: 18.197, three: 17.981 } },
   { name: "The Gloomroot Swamp", dataVer: "4", stars: { one: 10.75, two: 10.089, three: 10.034 } },
@@ -24,29 +24,31 @@ export const swiftMaps: SwiftMap[] = [
   { name: "Blossom Hills", dataVer: "14", stars: { one: 14, two: 13.593, three: 13.515 } },
   { name: "Amethyst Planet", dataVer: "18", stars: { one: 11.6, two: 10.94, three: 10.878 } },
   { name: "Pumpkin Patch", dataVer: "21", stars: { one: 9.8, two: 9.27, three: 9.185 } },
-  { name: "Scarlet Groove", dataVer: "22", stars: { one: 13.6, two: 12.588, three: 12.513 } },
+  { name: "Scarlet Grove", dataVer: "22", stars: { one: 13.6, two: 12.588, three: 12.513 } },
   { name: "Bluebell Gorge", dataVer: "23", stars: { one: 12.9, two: 12.477, three: 12.43 } },
 
   { name: "Aether", dataVer: "24", stars: { one: 18.25, two: 17.242, three: 17.071 } },
-  { name: "Grassy Islands", dataVer: "25", stars: { one: 16.3, two: 15.172, three: 15.126 } },
+  { name: "Grassy Islands", dataVer: "25", stars: { one: 16.3, two: 15.172, three: 15.116 } },
   { name: "Sandstone Steps", dataVer: "26", stars: { one: 15.1, two: 14.78, three: 14.655 } },
   { name: "Volcanic River", dataVer: "28", stars: { one: 22.7, two: 21.292, three: 21.05 } },
-  { name: "Acid Wasteland", dataVer: "29", stars: { one: 13.4, two: 12.298, three: 12.222 } },
+  { name: "Acid Wasteland", dataVer: "29", stars: { one: 13.4, two: 12.298, three: 12.172 } },
   { name: "Icy Cavern", dataVer: "30", stars: { one: 12.9, two: 12.398, three: 12.309 } },
   { name: "Crimson Timberland", dataVer: "31", stars: { one: 21.9, two: 19.309, three: 19.16 } },
-  { name: "Azulae Steeple", dataVer: "32", stars: { one: 15.9, two: 15.461, three: 15.359 } },
+  { name: "Azulae Steeple", dataVer: "32", stars: { one: 15.9, two: 15.461, three: 15.344 } },
 
-  { name: "Flamingo Ridgeway", dataVer: "33", stars: { one: 14.5, two: 14.107, three: 14.019 } },
+  { name: "Flamingo Ridgeway", dataVer: "33", stars: { one: 14.5, two: 14.107, three: 13.984 } },
   { name: "Chocolate Canyon", dataVer: "19", stars: { one: 17.1, two: 16.254, three: 16.106 } },
-  { name: "Achromatic Zone", dataVer: "36", stars: { one: 24.85, two: 23.39, three: 23.228 } },
+  { name: "Achromatic Zone", dataVer: "36", stars: { one: 24.85, two: 23.39, three: 23.178 } },
   { name: "Polychrome Cliffs", dataVer: "37", stars: { one: 13.1, two: 12.301, three: 12.186 } },
-  { name: "Reflective Realm", dataVer: "38", stars: { one: 19.2, two: 18.211, three: 18.08 } },
-  { name: "Strawberry Hills", dataVer: "39", stars: { one: 21.6, two: 17.309, three: 17.174 } },
+  { name: "Reflective Realm", dataVer: "38", stars: { one: 19.2, two: 18.211, three: 18.06 } },
+  { name: "Strawberry Hills", dataVer: "39", stars: { one: 21.6, two: 17.309, three: 17.094 } },
   { name: "Cherry Forest", dataVer: "46", stars: { one: 15.3, two: 14.106, three: 13.961 } },
   { name: "Pineapple Beach", dataVer: "43", stars: { one: 18.3, two: 17.847, three: 17.735 } },
 
   { name: "Mystical World", dataVer: "17", stars: { one: 36.1, two: 30.733, three: 30.356 } },
   { name: "Sandquick Sahara", dataVer: "16", stars: { one: 13.1, two: 12.81, three: 12.649 } },
-  { name: "Pollination Path", dataVer: "40", stars: { one: 19.6, two: 18.97, three: 18.866 } },
-  { name: "Abandoned Factory", dataVer: "42", stars: { one: 21.3, two: 19.976, three: 19.814 } },
+  { name: "Pollination Path", dataVer: "40", stars: { one: 19.6, two: 18.97, three: 18.796 } },
+  { name: "Abandoned Factory", dataVer: "42", stars: { one: 21.3, two: 19.976, three: 19.714 } },
+
+  { name: "Retro Racetrack", dataVer: "13", stars: { one: 24.6, two: 21.949, three: 21.671 } },
 ]
